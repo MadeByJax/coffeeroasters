312a08aa19d2cd625f8ad73b8e0b6387d031d5ce
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-12-22 20:01:05 +1000

    add: Component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Collection from "../components/Collection";
+import WhyChooseUs from "../components/WhyChooseUs";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Navbar />
       <Header />
       <Collection />
+      <WhyChooseUs />
     </div>
   );
 };

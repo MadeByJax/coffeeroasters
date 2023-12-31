180e8ab60c472f5dced0095cd24d773fb370312d
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-12-31 10:46:12 +1000

    add HowItWorks and Footer components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,18 @@ import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import Collection from "../components/Collection";
 import WhyChooseUs from "../components/WhyChooseUs";
+import HowItWorks from "../components/HowItWorks";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="bg-light-cream">
+    <div className="bg-light-cream pb-[72px]">
       <Navbar />
       <Header />
       <Collection />
       <WhyChooseUs />
+      <HowItWorks />
+      <Footer />
     </div>
   );
 };

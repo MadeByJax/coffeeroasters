32d31b8346df4ef5969a6ac003ee897ceecd513b
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2023-12-31 10:46:27 +1000

    update style

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -27,8 +27,8 @@ const WhyChooseUs = () => {
 
   return (
     <div className="px-6 mt-[120px] container relative mx-auto">
-      <div className="relative">
-        <div className="bg-2C343E h-[900px] absolute left-0 right-0"></div>
+      <div className="relative h-full">
+        <div className="bg-2C343E h-3/5 lg:h-4/5 rounded-lg  absolute left-0 right-0"></div>
         <div className="px-6 pt-16 relative text-center text-light-cream">
           <h2 className="text-[28px] font-bold ">Why choose us?</h2>
           <p className="mt-6 opacity-80">
@@ -38,28 +38,29 @@ const WhyChooseUs = () => {
             level.
           </p>
         </div>
-      </div>
-      <div className="relative rounded-b-[10px] pt-[36px] px-6 md:px-[58px] lg:px-[85px] md:pt-[58px] flex flex-col gap-6 lg:flex-row">
-        {reasonsToChooseUs.map((reason, index) => (
-          <div
-            key={index}
-            className="bg-dark-cyan flex flex-col items-center justify-center gap-14 rounded-lg px-[33px] pt-[72px] pb-[51px] md:flex-row md:py-[41px] md:px-[70px] lg:flex-col lg:px-10 lg:py-12 lg:mb-0"
-          >
-            <img
-              className="h-[72px] w-[72px] md:h-[56px] md:w-[56px] lg:h-[72px] lg:w-[72px] lg:mt-6"
-              src={reason.img}
-              alt={`Reason ${index + 1}`}
-            />
-            <div className="flex flex-col gap-4">
-              <h1 className="text-light-cream text-center  text-2xl font-bold leading-8 md:text-left lg:text-center">
-                {reason.name}
-              </h1>
-              <p className="text-light-cream text-center text-[15px] leading-[25px] md:text-left lg:text-center">
-                {reason.description}
-              </p>
+
+        <div className="relative rounded-b-[10px] pt-[36px] px-6 md:px-[58px] lg:px-[85px] md:pt-[58px] flex flex-col gap-6 lg:flex-row">
+          {reasonsToChooseUs.map((reason, index) => (
+            <div
+              key={index}
+              className="bg-dark-cyan flex flex-col items-center justify-center gap-14 rounded-lg px-[33px] pt-[72px] pb-[51px] md:flex-row md:py-[41px] md:px-[70px] lg:flex-col lg:px-10 lg:py-12 lg:mb-0"
+            >
+              <img
+                className="h-[72px] w-[72px] md:h-[56px] md:w-[56px] lg:h-[72px] lg:w-[72px] lg:mt-6"
+                src={reason.img}
+                alt={`Reason ${index + 1}`}
+              />
+              <div className="flex flex-col gap-4">
+                <h1 className="text-light-cream text-center  text-2xl font-bold leading-8 md:text-left lg:text-center">
+                  {reason.name}
+                </h1>
+                <p className="text-light-cream text-center text-[15px] leading-[25px] md:text-left lg:text-center">
+                  {reason.description}
+                </p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );

0e7487dbfe391e82e6064d95893dbd71b20195bd
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2024-01-26 16:02:59 +1000

    Refactor HowItWorks component to conditionally render elements based on props

--- src/components/HowItWorks.jsx
+++ src/components/HowItWorks.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Circle from "/src/assets/home/desktop/circle.svg";
 
-const HowItWorks = () => {
+const HowItWorks = ({ subscribePage }) => {
   const steps = [
     {
       step: "01",
@@ -24,11 +24,19 @@ const HowItWorks = () => {
   ];
 
   return (
-    <div className="px-6 mt-[120px] md:mt-[200px] container flex flex-col justify-center items-center md:items-start relative mx-auto">
-      <h1 className=" text-grey text-center text-2xl font-bold leading-8  md:text-left ">
-        How it works
-      </h1>
-      <div className="hidden md:flex items-center mt-[40px]">
+    <div
+      className={`flex flex-col justify-center items-center md:items-start relative mx-auto ${
+        subscribePage &&
+        " bg-2C343E rounded-[10px] px-[40px] lg:px-[85px] py-[100px]"
+      }`}
+    >
+      {!subscribePage && (
+        <h1 className="text-grey text-center text-2xl font-bold leading-8 md:text-left">
+          How it works
+        </h1>
+      )}
+
+      <div className="hidden md:flex items-center">
         <img src={Circle} alt="" />
         <span className="bg-pale-orange w-[250px] lg:w-[350px] h-[1px]"></span>
         <img src={Circle} alt="" />
@@ -36,25 +44,37 @@ const HowItWorks = () => {
         <img src={Circle} alt="" />
       </div>
 
-      <div className="flex flex-col md:flex-row md:gap-24">
+      <div className="flex flex-col md:flex-row gap-6 mt-[62px] md:gap-24">
         {steps.map((step, index) => (
           <div
             key={index}
-            className="mt-[56px] md:max-w-[285px] flex flex-col text-center md:text-left"
+            className="md:max-w-[285px] flex flex-col text-center md:text-left"
           >
             <h2 className="font-bold text-[72px] text-pale-orange">
               {step.step}
             </h2>
-            <h3 className="font-bold text-[28px] md:max-w-[230px] lg:text-[32px] text-grey-blue">
+            <h3
+              className={`font-bold text-[28px] md:max-w-[230px] md:text-[32px] ${
+                subscribePage ? "text-white" : "text-grey-blue"
+              }`}
+            >
               {step.name}
             </h3>
-            <p className="mt-6 text-grey-blue">{step.description}</p>
+            <p
+              className={`mt-6 ${
+                subscribePage ? "text-white" : "text-grey-blue"
+              }`}
+            >
+              {step.description}
+            </p>
           </div>
         ))}
       </div>
-      <button className="bg-dark-cyan w-[217px]  text-light-cream font-bold px-6 py-3 rounded-md mt-11">
-        Create your plan
-      </button>
+      {!subscribePage && (
+        <button className="bg-dark-cyan w-[217px]  text-light-cream font-bold px-6 py-3 rounded-md mt-11">
+          Create your plan
+        </button>
+      )}
     </div>
   );
 };

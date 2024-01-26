a0b3cd1f340b7f61a559878b16507c8c266af561
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2024-01-26 16:02:43 +1000

    Add price options and modal component

--- src/components/Selection.jsx
+++ src/components/Selection.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { React, useState, useEffect, useRef } from "react";
 import Arrow from "/src/assets/plan/desktop/icon-arrow.svg";
 import "./Selection.css";
+import OrderSummary from "./OrderSummary";
+import Modal from "./Modal";
 
 const Selection = () => {
   const QuestionCategories = [
@@ -65,18 +67,33 @@ const Selection = () => {
           title: "250g",
           description:
             "Perfect for the solo drinker. Yields about 12 delicious cups.",
+          price: {
+            "Every week": 7.2,
+            "Every 2 weeks": 9.6,
+            "Every month": 12.0,
+          },
         },
         {
           id: 8,
           title: "500g",
           description:
             "Perfect option for a couple. Yields about 40 delectable cups.",
+          price: {
+            "Every week": 13.0,
+            "Every 2 weeks": 17.5,
+            "Every month": 22.0,
+          },
         },
         {
           id: 9,
           title: "1000g",
           description:
             "Perfect for offices and events. Yields about 90 delightful cups.",
+          price: {
+            "Every week": 22.0,
+            "Every 2 weeks": 32.5,
+            "Every month": 42.0,
+          },
         },
       ],
     },
@@ -125,60 +142,153 @@ const Selection = () => {
     },
     // Add more questions and options as needed
   ];
+
   const [visibleOptions, setVisibleOptions] = useState(
     Array(CoffeeOptions.length).fill(true)
   );
   const [selectedOptions, setSelectedOptions] = useState(
-    Array(CoffeeOptions.length).fill(null)
+    Array(CoffeeOptions.length).fill({ title: "_____" })
   );
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const [isCapsuleSelected, setIsCapsuleSelected] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const handleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const calculatePrice = () => {
+    const weightOption = selectedOptions[2];
+    const deliveryOption = selectedOptions[4];
+    const weight = weightOption ? weightOption.title : null;
+    const frequency = deliveryOption ? deliveryOption.title : null;
+    console.log(weight, frequency);
+
+    // Check if weight and frequency are selected
+    if (weight && frequency) {
+      // Find the corresponding coffee option
+      const coffeeOption = CoffeeOptions[2].options.find(
+        (option) => option.title === weight
+      );
+
+      // Check if the coffee option and its prices are available
+      if (coffeeOption && coffeeOption.price && coffeeOption.price[frequency]) {
+        return coffeeOption.price[frequency] * 4;
+      }
+    }
+
+    // Return null if any condition is not met
+    return null;
+  };
 
   const toggleOptions = (index) => {
     const newVisibleOptions = [...visibleOptions];
     newVisibleOptions[index] = !newVisibleOptions[index];
     setVisibleOptions(newVisibleOptions);
+    console.log(visibleOptions);
   };
 
-  const handleOptionSelect = (categoryIndex, optionId) => {
-    const newSelectedOptions = [...selectedOptions];
-    newSelectedOptions[categoryIndex] = optionId;
-    setSelectedOptions(newSelectedOptions);
-    console.log(selectedOptions);
+  const handleOptionSelect = (categoryIndex, option) => {
+    setSelectedOptions((prevSelectedOptions) => {
+      const newSelectedOptions = [...prevSelectedOptions];
+      newSelectedOptions[categoryIndex] = option;
+      setSelectedCategory(QuestionCategories[categoryIndex]);
+
+      if (categoryIndex === 0 && option.title === "Capsule") {
+        setIsCapsuleSelected(true);
+        const newVisibleOptions = [...visibleOptions];
+        newVisibleOptions[3] = false; // Directly set the value
+        setVisibleOptions(newVisibleOptions);
+
+        console.log(selectedCategory);
+      }
+
+      if (categoryIndex === 0 && option.title !== "Capsule") {
+        setIsCapsuleSelected(false);
+        const newVisibleOptions = [...visibleOptions];
+        newVisibleOptions[3] = true; // Directly set the value
+        setVisibleOptions(newVisibleOptions);
+      }
+
+      if (categoryIndex !== 0) {
+        console.log(selectedCategory);
+      }
+
+      if (categoryIndex === 2 && option.price) {
+        newSelectedOptions[4] = option.price;
+      }
+
+      return newSelectedOptions; // Moved to the end
+    });
   };
+
+  console.log(isCapsuleSelected);
+
+  useEffect(() => {
+    handleOptionSelect(); // Call the function when the component mounts
+  }, []);
 
   return (
-    <div className="flex flex-col xl:flex-row justify-between">
-      <ul className="hidden font-bold text-[40px] xl:flex flex-col gap-[24px]">
+    <div className="flex flex-col xl:flex-row justify-between ">
+      <ul className="hidden font-bold text-[24px] xl:flex flex-col gap-[24px]">
         {QuestionCategories.map((category, index) => (
-          <li key={index}>
-            <h3 className="mb-[24px]">
-              <span>{String(index + 1).padStart(2, "0")}</span> {category}
-            </h3>
-            <div className="h-[1px] w-full bg-black"></div>
-          </li>
+          <a href={`#${category}`}>
+            <li
+              key={index}
+              className={
+                category === selectedCategory ? "text-black" : "text-gray-500"
+              }
+              onClick={() => setSelectedCategory(category)}
+            >
+              <h3 className="mb-[24px]">
+                <span>{String(index + 1).padStart(2, "0")}</span> {category}
+              </h3>
+              <div className="h-[1px] w-full bg-black"></div>
+            </li>
+          </a>
         ))}
       </ul>
-      <div className="flex flex-col gap-[96px]">
+      <div className="flex flex-col gap-[96px] max-w-[600px]">
         {CoffeeOptions.map((questionObj, categoryIndex) => (
           <div
+            id={QuestionCategories[categoryIndex]}
             key={questionObj.question}
             className={`fade-in ${
               visibleOptions[categoryIndex] ? "visible" : ""
             }`}
           >
-            <div
-              className="flex items-center justify-between"
-              onClick={() => toggleOptions(categoryIndex)}
-            >
-              <h2 className="text-app-grey text-[24px] lg:text-[40px] font-bold">
+            <div className="flex items-center justify-between">
+              <h2
+                className={`text-app-grey text-[24px] lg:text-[40px] font-bold ${
+                  isCapsuleSelected && categoryIndex === 3 ? "unavailable" : ""
+                }`}
+              >
                 {questionObj.question}
               </h2>
-              <img
-                className={`w-[18px] h-[12px] transform ${
-                  visibleOptions[categoryIndex] ? "rotate-180" : "rotate-0"
-                }`}
-                src={Arrow}
-                alt=""
-              />
+
+              {categoryIndex !== 3 && (
+                <img
+                  className={`w-[18px] h-[12px] transform ${
+                    visibleOptions[categoryIndex] ? "rotate-180" : "rotate-0"
+                  }`}
+                  onClick={() => toggleOptions(categoryIndex)}
+                  src={Arrow}
+                  alt=""
+                />
+              )}
+
+              {categoryIndex === 3 && !isCapsuleSelected && (
+                <img
+                  className={`w-[18px] h-[12px] transform ${
+                    visibleOptions[categoryIndex] ? "rotate-180" : "rotate-0"
+                  }`}
+                  onClick={() => toggleOptions(categoryIndex)}
+                  src={Arrow}
+                  alt=""
+                />
+              )}
             </div>
             {visibleOptions[categoryIndex] && (
               <div className="mt-[32px] flex flex-col md:flex-row gap-[16px]">
@@ -186,11 +296,11 @@ const Selection = () => {
                   <div
                     key={option.id}
                     className={` cursor-pointer p-[24px] md:w-[228px] md:h-[250px] text-grey-blue ${
-                      selectedOptions[categoryIndex] === option.id
+                      selectedOptions[categoryIndex].id === option.id
                         ? "bg-dark-cyan text-white"
                         : "bg-app-off-white"
                     }`}
-                    onClick={() => handleOptionSelect(categoryIndex, option.id)}
+                    onClick={() => handleOptionSelect(categoryIndex, option)}
                   >
                     <h3 className="font-bold text-[24px]">{option.title}</h3>
                     <p>{option.description}</p>
@@ -200,7 +310,21 @@ const Selection = () => {
             )}
           </div>
         ))}
+        <OrderSummary
+          selectedOptions={selectedOptions}
+          CoffeeOptions={CoffeeOptions}
+          handleModal={handleModal}
+          calculatePrice={calculatePrice}
+          isCapsuleSelected={isCapsuleSelected}
+        />
       </div>
+      {isModalOpen && (
+        <Modal
+          calculatePrice={calculatePrice}
+          selectedOptions={selectedOptions}
+          handleModal={handleModal}
+        />
+      )}
     </div>
   );
 };

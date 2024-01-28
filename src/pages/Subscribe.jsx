d2340dc84665125b71d4268d194eb7e01984918e
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2024-01-28 13:49:25 +1000

    Refactor Subscribe page layout

--- src/pages/Subscribe.jsx
+++ src/pages/Subscribe.jsx
@@ -7,25 +7,19 @@ import Selection from "../components/Selection";
 
 const Subscribe = () => {
   return (
-    <div className="pb-[72px]">
-      {/* Navbar and SubscribeHero with consistent padding */}
-
-      <div className="container mx-auto bg-light-cream px-6">
+    <div className="bg-light-cream">
+      <div className="container mx-auto px-6 pb-[72px] ">
         <Navbar />
         <SubscribeHero />
-      </div>
-
-      {/* HowItWorks with consistent padding */}
-
-      <div className=" rounded-[10px] mt-[155px] lg:px-6 lg:container mx-auto">
-        <HowItWorks subscribePage={true} />
-      </div>
-      <div className="mt-[120px] px-6 lg:px-20 container mx-auto">
-        <Selection />
-      </div>
-      {/* Footer with consistent padding */}
-      <div className="mt-36 px-6 container mx-auto">
-        <Footer />
+        <div className="rounded-[10px] mt-32">
+          <HowItWorks subscribePage={true} />
+        </div>
+        <div className="mt-32 xl:px-12">
+          <Selection />
+        </div>
+        <div className="mt-32">
+          <Footer />
+        </div>
       </div>
     </div>
   );

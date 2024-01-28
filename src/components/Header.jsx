dad9a13c73fde91c1fd27d2ef269b9e94f9d054b
MadeByJax <134830936+MadeByJax@users.noreply.github.com>
2024-01-28 13:50:08 +1000

    Add Link component to create plan button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="px-6 container mx-auto">
+    <div className="">
       <div className="flex flex-col text-center sm:text-left rounded-lg px-6 sm:px-[86px] justify-center items-center sm:items-start bg-cover bg-center bg-no-repeat h-[500px] md:h-[600px] bg-[url('src/assets/home/mobile/image-hero-coffeepress.jpg')] sm:bg-[url('src/assets/home/desktop/image-hero-coffeepress.jpg')]">
         <div className="max-w-[500px] flex flex-col gap-6">
           <h1 className="text-light-cream font-bold text-[40px] leading-10 sm:text-[48px] md:text-[72px] md:leading-[72px]">
@@ -13,9 +14,11 @@ const Header = () => {
             curated artisan coffees from our best roasters delivered directly to
             your door, at your schedule.
           </p>
-          <button className="bg-dark-cyan text-light-cream font-bold px-6 py-3 rounded-md md:w-1/2">
-            Create your plan
-          </button>
+          <Link to="/subscribe">
+            <button className="bg-dark-cyan text-light-cream font-bold px-6 py-3 rounded-md md:w-1/2">
+              Create your plan
+            </button>
+          </Link>
         </div>
       </div>
     </div>
